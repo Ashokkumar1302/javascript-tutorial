22202386333d1cd2567210909dc798d3832299c4
Ashokkumar1302 <ashokvishwakarma1310@gmail.com>
2024-11-17 00:02:57 +0530

    Data type

---
+++ variable.js
@@ -0,0 +1,13 @@
+let ans;
+console.log(ans);   // out put =>undefined
+console.log( typeof(NaN))  //NaN
+//type of NaN =>number
+
+console.log(null); //null
+console.log(typeof(null));
+//type of null =>object
+
+//important
+
+console.log((NaN**0)) // =>NaN^0 =>1
+
